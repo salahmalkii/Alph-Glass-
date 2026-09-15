205804776cc42f9b4ab23ff789a1894667307a02
salahmalkii <malkisalah95@gmail.com>
2026-09-16 00:32:56 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'alpha-glass-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
